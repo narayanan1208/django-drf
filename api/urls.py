41c2888b5801b12f5aaffa87506607e858937c2c
narayanan1208 <narayanan6647@gmail.com>
2025-07-01 16:42:12 +0530

    Added views, urls and serializer for employee model.

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("students/", views.studentsView),
     path("students/<int:pk>/", views.studentDetailView),
+    # as_view() identifies it as class based views
+    path("employees/", views.Employees.as_view()),
+    path("employees/<int:pk>/", views.EmployeeDetail.as_view()),
 ]
