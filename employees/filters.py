8edd0e5bd3e3e7d1e2b343d5a32deaebf9061a98
narayanan1208 <narayanan6647@gmail.com>
2025-07-05 13:16:46 +0530

    Advanced filter example

--- employees/filters.py
+++ employees/filters.py
@@ -9,8 +9,17 @@ class EmployeeFilter(django_filters.FilterSet):
     )
     emp_name = django_filters.CharFilter(field_name="emp_name", lookup_expr="icontains")
     # Range filter only works on interger field or priary key.
-    id = django_filters.RangeFilter(field_name="id")
+    # id = django_filters.RangeFilter(field_name="id")
+    id_min = django_filters.CharFilter(method="filter_by_id_range", label="From EMP ID")
+    id_max = django_filters.CharFilter(method="filter_by_id_range", label="To EMP ID")
 
     class Meta:
         model = Employee
-        fields = ["designation", "emp_name", "id"]
+        fields = ["designation", "emp_name", "id_min", "id_max"]
+
+    def filter_by_id_range(self, queryset, name, value):
+        if name == "id_min":
+            return queryset.filter(emp_id__gte=value)
+        elif name == "id_max":
+            return queryset.filter(emp_id__lte=value)
+        return queryset
